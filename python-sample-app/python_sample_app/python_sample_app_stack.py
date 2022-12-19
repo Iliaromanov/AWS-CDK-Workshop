ba047f86209b5a3354285d8918cfb8cb24ce83cb
Ilia Romanov <iliaromanov033@gmail.com>
2022-12-19 14:46:22 -0500

    updating stack to create hitcounter lambda and pass it our hello lambda to be invoked downstream and pointing apigw to call hitcouter instead

--- python-sample-app/python_sample_app/python_sample_app_stack.py
+++ python-sample-app/python_sample_app/python_sample_app_stack.py
@@ -1,23 +1,29 @@
 from constructs import Construct
 from aws_cdk import (
     Stack,
-    aws_lambda,
+    aws_lambda as _lambda,
     aws_apigateway as apigw,
 )
 
+from .hitcounter import HitCounter
 
 class PythonSampleAppStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        my_lambda = aws_lambda.Function(
+        my_lambda = _lambda.Function(
             self, "HelloLambda",
-            runtime=aws_lambda.Runtime.PYTHON_3_7,
-            code=aws_lambda.Code.from_asset("lambda"),
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.from_asset("lambda"),
             handler="hello.handler"
+        )
+
+        hello_with_hitcounter = HitCounter(
+            self, 'HitCounter',
+            downstream=my_lambda,
         )
 
         apigw.LambdaRestApi(
             self, "Endpoint",
-            handler=my_lambda
+            handler=hello_with_hitcounter.handler
         )

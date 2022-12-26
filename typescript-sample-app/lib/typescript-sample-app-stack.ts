8d7bf2377c97d31fdb5a0cf4067c1ec8708c16a1
Ilia Romanov <iliaromanov033@gmail.com>
2022-12-25 21:52:50 -0500

    creating hello lambda

--- typescript-sample-app/lib/typescript-sample-app-stack.ts
+++ typescript-sample-app/lib/typescript-sample-app-stack.ts
@@ -1,19 +1,17 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
 
 export class TypescriptSampleAppStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'TypescriptSampleAppQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    // Define AWS Lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler'
     });
-
-    const topic = new sns.Topic(this, 'TypescriptSampleAppTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
